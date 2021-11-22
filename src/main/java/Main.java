1e4f3400d140e99a1e74da60fcd50c3931d0cb1a
Vinita Maloo <vinitamaloo@yahoo.in>
2021-11-21 19:36:53 -0700

    Initial commit for setup

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+
+/**
+ * @author Vinita Maloo(vmaloo@asu.edu)
+ */
+
+public class Main {
+
+    Model _friends = null;
+    Model schema = null;
+    InfModel inferredFriends = null;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
