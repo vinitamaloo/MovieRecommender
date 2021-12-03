71b7a1773ebb9f535788a48590cbaee421ffe8af
Brusuvain <42177208+Brusuvain@users.noreply.github.com>
2021-12-02 17:43:18 -0700

    Collaborative query Returning recommended movie IDS

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.ResultSet;
@@ -19,6 +23,8 @@ public class Main {
 		services.getMovieDetails("100");
 		services.getPopularMovies();
     	contentBasedFiltering();
+		System.out.println("\n\n\n\nCCCCOOOOOOLLLLLLABBBBBBBBBBBBBBBBB\n\n\n");
+		collborativeFiltering();
     }
 
     public static void contentBasedFiltering() 
@@ -38,6 +44,99 @@ public class Main {
        	
        	loadTest(queryString);  
     }
+
+	public static void collborativeFiltering() 
+    {
+    	String query = "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
+		+"\nPREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+		+"\nPREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+		+"\nPREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+		+"\nPREFIX use: <http://www.semanticweb.org/ontologies/2021/10/untitled-ontology-53#>"
+		+"\nSelect DISTINCT ?movieid3 ?userid2"
+		+"\n	WHERE{"
+		+"\n	  ?user rdf:type use:User."
+		+"\n	  ?user use:userid ?userid2."
+		+"\n	  ?user use:has_rated ?rated."
+		+"\n	  ?rated use:movieid ?movieid3."
+		+"\n	  ?rated use:rating ?rating"
+		+"\n	  {Select ?userid ?movieid ?userid3"
+			+"\n		  WHERE {"
+				+"\n	  ?user rdf:type use:User."
+				+"\n  ?user use:userid ?userid."
+				+"\n  ?user use:has_rated ?rated."
+				+"\n  ?rated use:movieid ?movieid2."
+				+"\n  ?rated use:rating ?rating"
+				+"\n  {SELECT ?movieid ?movieid ?userid3"
+					+"\n	WHERE {"
+						+"\n	?user rdf:type use:User."
+						+"\n	?user use:userid ?userid3."
+						+"\n	?user use:has_rated ?rated."
+						+"\n	?rated use:movieid ?movieid."
+						+"\n	?rated use:rating ?rating"
+						+"\n	FILTER(?userid3 = 2 && ?rating = 5 )."
+						+"\n	}"
+						+"\n	LIMIT 3"
+						+"\n	  }"
+						+"\n	FILTER (?movieid2 IN (?movieid) && ?userid != 2 && ?rating = 5)."
+						+"\n  }"
+						+"\n  LIMIT 5"
+						+"\n  }"
+		  
+						+"\nFILTER (?userid2 IN (?userid)  && ?movieid3 NOT IN (?movieid) && ?rating = 5)."
+		  
+						+"\n}"
+						+"\nLIMIT 100";
+       	collborativeFilteringSupport(query);
+       	 
+    }
+
+	public static List<Integer> collborativeFilteringSupport(String query){
+		QueryExecution qexec = QueryExecutionFactory.sparqlService(serviceEndPoint,query);	
+		 ResultSet results = qexec.execSelect();
+		String s= ResultSetFormatter.asText(results);
+//		System.out.println(s.substring(69,92));
+//		System.out.println(s.substring(71,74));
+//		System.out.println(s.substring(82,85));
+//		System.out.println(s);
+		int left=71;
+		int leftOfId=82;
+		String key=s.substring(82,85);
+		HashMap<String,List<Integer>> eachUserWithTheir3Recommendations=new HashMap<>();
+		while(left<s.length()-50)
+		{
+			int right=0;
+	            right=leftOfId+getNumberOfDigits(leftOfId,s);
+	           // System.out.print(s.substring(leftOfId,right));
+				key=s.substring(leftOfId,right);
+				leftOfId+=23;
+				right=left+getNumberOfDigits(left,s);
+				//System.out.println(" <--------    "+s.substring(left, right));
+				List<Integer> temp=eachUserWithTheir3Recommendations.getOrDefault(key, new ArrayList<>());
+				temp.add(Integer.valueOf(s.substring(left, right)));
+				eachUserWithTheir3Recommendations.put(key,temp);
+				left+=23;
+		}
+		System.out.println(eachUserWithTheir3Recommendations.keySet());
+		List<Integer> finalRecommendations=new ArrayList<>();
+		for(String keyIter:eachUserWithTheir3Recommendations.keySet()) {
+			List<Integer> movieRecommendations=eachUserWithTheir3Recommendations.get(keyIter);
+			for(int i=0;i<movieRecommendations.size()||i<3;i++) {
+				finalRecommendations.add(movieRecommendations.get(i));
+			}
+		}
+		return finalRecommendations;
+}
+	
+    
+	private static int getNumberOfDigits(int left, String s) {
+		int count=0;
+		while(Character.isDigit(s.charAt(left))) {
+			++count;
+			++left;
+		}
+		return count;
+	}
+	
     
 	public static void loadTest(String query) {
 		
