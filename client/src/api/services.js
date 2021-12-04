a29c987e23f674cb088b0d2fc749ef21680f8d0c
Vinita S. Maloo <vinitamaloo@yahoo.in>
2021-12-04 00:24:20 -0700

    Add support to send

--- client/src/api/services.js
+++ client/src/api/services.js
@@ -4,7 +4,7 @@ import { useParams } from 'react-router';
 const serverUrl = 'http://localhost:8080/server/'
 
 export const get_popular_movies = () => { return axios.get(serverUrl + 'get_popular_movies') };
-export const get_movie_details = () => { return axios.get(serverUrl + 'get_movie_details') };
+export const get_movie_details = (movie_id) => { return axios.get(serverUrl + 'get_movie_details/'+movie_id) };
 export const get_recommendation_by_user = (userId) => { return axios.get(serverUrl + 'get_recommendation_by_Users_rating/'+userId)};
 export const get_recommendation = (userId) => {
                 return axios.get(serverUrl +'get_recommendation_based_on_other_users/'+userId)};
